2a9d6d22f0f078f118d867955b589f62e48517f9
sinna3133-cloud <sinna3133@gmail.com>
2026-03-21 11:30:18 -0400

    Create kakao-auth.js

---
+++ api/kakao-auth.js
@@ -0,0 +1,8 @@
+const KAKAO_CLIENT_ID = ''; // ← 여기에 카카오 JavaScript 키 입력
+```
+
+**4. Vercel 환경변수에 추가 (이미 했으면 확인)**
+```
+KAKAO_CLIENT_ID     = JavaScript 키
+KAKAO_CLIENT_SECRET = REST API 키
+KAKAO_REDIRECT_URI  = https://[앱주소]/
